8e046cbe719d535a0a475fc1501c57d1dc2d4d09
Imon123gg <124933030+Imon123gg@users.noreply.github.com>
2023-03-07 23:26:51 +0300

    Create Configuration.java

---
+++ Configuration.java
@@ -0,0 +1,29 @@
+package future.code.dark.dungeon.config;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public interface Configuration {
+
+    String GAME_NAME="Dark Dungeon";
+    String MAP_FILE_PATH = "src/main/resources/maps/map.ber";
+    Boolean ENEMIES_ACTIVE = false;
+    int MILLISECONDS_PER_FRAME = 40;
+    char WALL_CHARACTER = '1';
+    char EXIT_CHARACTER = 'E';
+    char LAND_CHARACTER = '0';
+    char PLAYER_CHARACTER = 'P';
+    char ENEMY_CHARACTER = 'G';
+    char COIN_CHARACTER = 'C';
+    Integer SPRITE_SIZE = 64;
+    String PLAYER_SPRITE = "src/main/resources/assets/hero/tile000.png";
+    String GHOST_SPRITE = "src/main/resources/assets/ghost/tile000.png";
+    String WALL_SPRITE = "src/main/resources/assets/land/wall.png";
+    String LAND_SPRITE = "src/main/resources/assets/land/ground.png";
+    String EXIT_SPRITE = "src/main/resources/assets/land/out.png";
+    String COIN_SPRITE = "src/main/resources/assets/land/collectible.png";
+    String VICTORY_SPRITE = "src/main/resources/assets/victory.jpg";
+    String MAP ="1111111111111111111CG100110C100010C1100000E101100010011001001100001010011001000000111000011000010110000010011011010C10111110111C00011010G01C00011001010000100001011101011110110111011C0100C00000C00P01111111111111111111";
+
+
+}
